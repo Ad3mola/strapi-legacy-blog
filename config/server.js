e25ebe50e8db1fce35ffc9314f61d229b21d8062
StefanoSalidu <st.salidu@gmail.com>
2021-05-05 15:34:41 +0200

    chore: restoring heroku default port

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env("ADMIN_HOST", "0.0.0.0"),
-  port: env('ADMIN_PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env('PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
